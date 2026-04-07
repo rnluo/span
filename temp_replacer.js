20b6632b1847ad8ec0edf9a8a54e9baf943ec9bd
rnluo <ning.l.rn@connect.hku.hk>
2026-04-07 12:18:23 +0800

    Clean temp files

--- temp_replacer.js
+++
@@ -1,4 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('src/App.jsx', 'utf-8');
-
-// I will overwrite all of it instead to avoid mess.
--- temp_update2.js
+++
@@ -1,1 +0,0 @@
-﻿// (We will skip the full script and instead ask the user to wait while I implement the major rewrite for dragging and resizing in a single action)
